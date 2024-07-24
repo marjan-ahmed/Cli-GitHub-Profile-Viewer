70991800b11b9714431b3164aa36b1b9e3a9499e
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-07-25 02:46:48 +0500

    updating package: enhanced UI

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-
-console.log(chalk.greenBright.bold("\n \tWelcome to CLI GitHub Profile Finder - Marjan Ahmed".toUpperCase()));
-console.log(chalk.green("-").repeat(67));
-console.log("\n");
-
-let user_input = await inquirer.prompt({
-    name: "username",
-    type: "input",
-    message: "Enter your GitHub username: "
-});
+import ora from 'ora';
+import figlet from 'figlet';
+import boxen from 'boxen';
 
 let fetchApi = async() => {
+
+    console.log(
+        boxen(figlet.textSync('GitHub Finder', { horizontalLayout: 'full' }), { padding: 1, margin: 1, borderStyle: 'double' })
+    );
+    console.log(chalk.green("-").repeat(100));
+    console.log("\n");
+    
+    let user_input = await inquirer.prompt({
+        name: "username",
+        type: "input",
+        message: "Enter your GitHub username: "
+    });
+
+
     const api: string = `https://api.github.com/users/${user_input.username}`;
-    console.log(chalk.cyan(`\nFetching data for: ${user_input.username}...\n`));
+    let spinner = ora(chalk.cyan(`Fetching data for: ${user_input.username}...\n`)).start();
 
     try {
         let fetchData = await fetch(api);
         let res = await fetchData.json();
+
+        spinner.succeed(`Data fetched for: ${user_input.username}`);
+        console.log(chalk.cyan('\nUser Profile Information:\n'));
+
         console.log("Username:",chalk.yellow(res.login));
         console.log("URL: ",chalk.yellow(res.html_url));
         console.log("User Name:",chalk.yellow(res.name));
@@ -27,6 +38,7 @@ let fetchApi = async() => {
         console.log("Number of Public Repos:",chalk.yellow(res.public_repos));
     }
     catch (err) {
+        spinner.fail('Error fetching data');
         console.error(chalk.red("Error fetching data:", err));
         return;
     }
